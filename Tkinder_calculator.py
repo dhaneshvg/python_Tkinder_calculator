ae5e4792a0d08dbde37565f4f94dfa5f4854b119
dhaneshvg <dhanesh.v.gddd@gmail.com>
2021-05-04 15:42:39 +0530

    function for the calculator created

--- Tkinder_calculator.py
+++ Tkinder_calculator.py
@@ -5,3 +5,27 @@ window.geometry("360x385")
 window.resizable(0, 0)
 window.configure(background="#696969")
 window.title("Calculator By DVG") 
+
+
+
+def btn_click(item):
+    global expression
+    expression = expression + str(item)
+    input_text.set(expression)
+
+
+def bt_clear():
+    global expression
+    expression = ""
+    input_text.set("")
+
+
+def bt_equal():
+    global expression
+    result = str(eval(expression))
+    input_text.set(result)
+    expression = ""
+
+
+expression = ""
+input_text = StringVar()
